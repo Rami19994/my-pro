7fc3a0a03aac0f182a56bb80f5160643e0b4ef19
Rami19994 <138163686+Rami19994@users.noreply.github.com>
2024-06-19 14:55:07 +0300

    Delete pro/pro1 directory

--- pro/pro1/fire.py
+++
@@ -1,5 +0,0 @@
-import firebase_admin
-from firebase_admin import credentials
-
-cred = credentials.Certificate("the-legend-e6853-firebase-adminsdk-arn02-0dc20fc57a.json")
-firebase_admin.initialize_app(cred)
